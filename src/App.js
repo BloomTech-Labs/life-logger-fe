e68ee0297fceb43f269f275912cfb56e16c3264d
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-17 16:05:27 -0500

    seeing if branch protection of master is working

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1> Hello World </h1>
+        <h2> Hello World </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
