476e10565e16c5bb0ffec3d0e8aff54f1fd6feac
Denvinn Magsino <denvinnmagsino@Denvinns-MacBook-Air.local>
2020-02-25 21:20:25 -0800

    login form in progress: login inputs completed

--- src/components/landing page/Login.js
+++ src/components/landing page/Login.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { connect } from "react-redux";
+import { fetchUser } from "../../actions/index"
 
+const initialValues = {
+  username: "",
+  password: ""
+}
 
-function Login() {
+const FetchUserForm = props => {
+  const [user, setUser] = useState(initialValues);
+
+  const handleChange = e => { setUser({...user, [e.target.name] : e.target.value}); };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.fetchUser(user)
+  };
+
   return (
-    <div >
-     <h2>This is the Login!</h2>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit = {handleSubmit}>
+        <div>
+          <div>
+            <input 
+              name = "username"
+              placeholder = "username or email"
+              value = {user.username}
+              onChange = {handleChange}
+            />
+          </div>
+          <div>
+            <input 
+              name = "password"
+              placeholder = "password"
+              type = "password"
+              value = {user.password}
+              onChange = {handleChange}
+            />
+          </div>
+          <button type = "submit">Submit</button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
 
-export default Login;
+
+
+export default connect(
+  state => {
+    return {
+      userData: state.userData,
+      isFetching: state.isFetching,
+      error: state.error
+    };
+  },
+  { fetchUser })
+  (FetchUserForm);
