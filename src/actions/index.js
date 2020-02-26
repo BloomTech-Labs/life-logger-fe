3c1fa90f5f854a83c81bde4869faeae7ba33aa83
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2020-02-25 22:31:30 -0600

    Got axios set up for registraition

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,66 @@
 
-export const NEW_TODO = 'NEW_TODO';
-export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
- export const addTodo = (todo) => {
-  return {
-    type: NEW_TODO,
-    payload: todo
-  };
+// Action Types
+export const FETCH_USER_LOADING = "FETCH_USER_LOADING";
+export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+
+export const CREATE_USER_START = "CREATE_USER_START";
+export const CREATE_USER_SUCCESS = "CREATE_USER_SUCCESS";
+export const CREATE_USER_FAILURE = "CREATE_USER_FAILURE";
+
+export const DELETE_USER_START = "DELETE_USER_START";
+export const DELETE_USER_SUCCESS = "DELETE_USER_SUCCESS";
+export const DELETE_USER_FAILURE = "DELETE_USER_FAILURE";
+
+export const UPDATE_USER_START = "UPDATE_USER_START";
+export const UPDATE_USER_SUCCESS = "UPDATE_USER_SUCCESS";
+export const UPDATE_USER_FAILURE = "UPDATE_USER_FAILURE";
+
+const host = `http://localhost:3000`;
+
+// Async action creators
+
+export const fetchUser = () => dispatch => {
+  dispatch({ type: FETCH_USER_LOADING });
+  axiosWithAuth()
+    .get(`${host}/api/users`)
+    .then(response => dispatch({ type: FETCH_USER_SUCCESS, payload: response.data }))
+    .catch(error => dispatch({ type: FETCH_USER_FAILURE, payload: error }));
 };
 
- export const toggleComplete = (index) => {
-  return {
-    type: TOGGLE_COMPLETE,
-    payload: index
-  };
+export const createUser = newUser => dispatch => {
+  dispatch({ type: CREATE_USER_START });
+  axiosWithAuth()
+    .post(`${host}/api/users/new`, newUser)
+    .then(response => {
+      console.log("Response from POST: ", response);
+      dispatch({ type: CREATE_USER_SUCCESS, payload: response.data });
+    })
+    .catch(error => dispatch({ type: CREATE_USER_FAILURE, payload: error }));
+};
+
+export const deleteUser = id => dispatch => {
+  console.log("deleteUser passed ID: ", id);
+  dispatch({ type: DELETE_USER_START });
+  axiosWithAuth()
+    .delete(`${host}/api/users/delete/${id}`)
+    .then(response => {
+      console.log("Delete User Response: ", response);
+      dispatch({ type: DELETE_USER_SUCCESS, payload: response });
+    })
+    .catch(error => dispatch({ type: DELETE_USER_FAILURE, payload: error }));
+};
+
+export const updateUser = (editedUser, id) => dispatch => {
+  console.log("editedUser getting passed in: ", id);
+  dispatch({ type: UPDATE_USER_START });
+  axiosWithAuth()
+    .put(`${host}/api/users/update/${id}`, editedUser)
+    .then(response => {
+      console.log("updateUser response: ", response);
+      dispatch({ type: UPDATE_USER_SUCCESS, payload: response.data });
+    })
+    .catch(error => dispatch({ type: UPDATE_USER_FAILURE, payload: error }));
 };
---
+++ src/reducers/RegisterReducer.js
@@ -0,0 +1,103 @@
+import {
+  FETCH_USER_LOADING,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+  CREATE_USER_START,
+  CREATE_USER_SUCCESS,
+  CREATE_USER_FAILURE,
+  DELETE_USER_START,
+  DELETE_USER_SUCCESS,
+  DELETE_USER_FAILURE,
+  UPDATE_USER_START,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILURE
+} from "../actions/index";
+
+const initialState = {
+  userData: [],
+  error: null,
+  isFetching: false
+};
+
+function RegisterReducer(state = initialState, action) {
+  console.log("Reducer Firing", action);
+  switch (action.type) {
+    case FETCH_USER_LOADING:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      };
+    case FETCH_USER_SUCCESS:
+      return {
+        ...state,
+        restData: action.payload,
+        error: null
+      };
+    case FETCH_USER_FAILURE:
+      return {
+        ...state,
+        restData: [],
+        isFetching: false,
+        error: action.payload
+      };
+    case CREATE_USER_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      };
+    case CREATE_USER_SUCCESS:
+      return {
+        ...state,
+        restData: [...state, action.payload],
+        error: null
+      };
+    case CREATE_USER_FAILURE:
+      return {
+        ...state,
+        restData: [],
+        error: action.payload
+      };
+    case DELETE_USER_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      };
+    case DELETE_USER_SUCCESS:
+      return {
+        ...state,
+        restData: [...state, action.payload],
+        error: null
+      };
+    case DELETE_USER_FAILURE:
+      return {
+        ...state,
+        restData: [],
+        error: action.payload
+      };
+    case UPDATE_USER_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      };
+    case UPDATE_USER_SUCCESS:
+      return {
+        ...state,
+        restData: [...state, action.payload],
+        error: null
+      };
+    case UPDATE_USER_FAILURE:
+      return {
+        ...state,
+        restData: [],
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+export default RegisterReducer;
