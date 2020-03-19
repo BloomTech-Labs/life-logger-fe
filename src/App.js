a0015b7ea5516c9b5e43918e1a0d3d6d5df22cc7
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-18 23:56:36 -0400

    adds route to Tast and EditTask component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import Dashboard from './components/dashboard';
-//Components
+import EditTask from './components/dashboard/components/EditTask';
+import Task from './components/dashboard/components/Task';
 import Header from './components/landing-page/components/Header';
 import LandingPage from './components/landing-page/index';
 import { AppContainer } from './styles/Styles';
 
-function App() {
+function App(props) {
   return (
     <AppContainer>
       <Header />
       <PrivateRoute exact path="/" component={Dashboard} />
       <Route path="/home" component={LandingPage} />
+      <Route path="/task/:id" component={Task} />
+      <Route path="/edit-task/:id" component={EditTask} />
     </AppContainer>
   );
 }
