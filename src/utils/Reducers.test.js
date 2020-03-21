beaeb466e632304440dcfaf0309c8f2aa706d08d
Carl-Stanley <cds.beml@gmail.com>
2020-03-21 10:31:42 -0700

    Commit

--- src/utils/Reducers.test.js
+++ src/utils/Reducers.test.js
@@ -6,11 +6,32 @@ describe('Events Reducer', () => {
   };
   const store =  createAStore();
   let newState = store.getState();
-  it('FETCH EVENTS', () => {
-    /// FETCH EVENTS LOADING
+   
+  /// FETCH EVENTS LOADING
+  it('FETCH EVENTS LOADING', () => {
+    
     store.dispatch({ type: 'FETCH_EVENTS_LOADING' });
     newState = store.getState();
     expect(newState.events.isFetching).toBe(true);
     expect(newState.events.error).toBe(null);
   });
+  
+  /// FETCH EVENTS SUCCESS
+  it('FETCH EVENTS SUCCESS', () => {
+    
+    store.dispatch({ type: 'FETCH_EVENTS_SUCCESS' });
+    newState = store.getState();
+    expect(newState.events.isFetching).toBe(false);
+    expect(newState.events.error).toBe(null);
+  });
+/*
+  /// FETCH EVENTS FAILURE
+  it('FETCH EVENTS FAILURE', () => {
+    
+    store.dispatch({ type: 'FETCH_EVENTS_FAILURE' });
+    newState = store.getState();
+    expect(newState.events.isFetching).toBe(false);
+    expect(newState.events.error).toBe(null);
+  });
+*/
 });
