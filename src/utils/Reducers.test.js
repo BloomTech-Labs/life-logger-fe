7995c0da81b854c3fc3c9d80381a36645f9ba32c
Carl-Stanley <cds.beml@gmail.com>
2020-03-21 10:18:29 -0700

    Commit

--- src/utils/Reducers.test.js
+++ src/utils/Reducers.test.js
@@ -4,7 +4,7 @@ describe('Events Reducer', () => {
   const expectedState = {
     title: 'Example title'
   };
-  const store = testStore();
+  const store =  createAStore();
   let newState = store.getState();
   it('FETCH EVENTS', () => {
     /// FETCH EVENTS LOADING
