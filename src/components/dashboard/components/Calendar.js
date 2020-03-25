1641d5c1dec2b8611ca3b9b79f6bc72e305bf233
Carl-Stanley <cds.beml@gmail.com>
2020-03-25 12:38:42 -0700

    Commit - example Calendar App.

---
+++ src/components/dashboard/components/Calendar.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import events from "./CalendarItem";
+import BigCalendar from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+moment.locale("en");
+BigCalendar.momentLocalizer(moment);
+
+class CalenderApp extends Component {
+    state = {
+      view: "day",
+      date: new Date(2015, 3, 12),
+      width: 500
+    };
+  
+    componentDidMount() {
+      window.addEventListener("resize", () => {
+      
+      });
+    }
+  
+    render() {
+      return (
+        <div style={{ height: 700 }}>
+          <button onClick={() => this.setState({ view: "day" })}>Day</button>
+          <button onClick={() => this.setState({ view: "month" })}>Month</button>
+          <BigCalendar
+            style={{ height: 500, width: this.state.width }}
+            toolbar={false}
+            events={events}
+            step={60}
+            views={allViews}
+            view={this.state.view}
+            onView={() => {}}
+            date={this.state.date}
+            onNavigate={date => this.setState({ date })}
+          />
+        </div>
+      );
+    }
+  }
+  
+  export default CalenderApp;
+  
