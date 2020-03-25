0a1f75a46f844b4e0985a67c6035f4dd6d92e490
Carl-Stanley <cds.beml@gmail.com>
2020-03-25 13:41:52 -0700

    Commit

--- src/components/dashboard/components/Calendar.js
+++ src/components/dashboard/components/Calendar.js
@@ -1,45 +1,24 @@
-import React, { Component } from "react";
-//import { render } from "react-dom";
-import events from "./CalendarItem";
-import {Calendar} from "react-big-calendar";
-//import moment from "moment";
+import React from "react";
+import BigCalendar from "react-big-calendar";
+import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import eventsList from "./CalendarItem";
 
-//moment.locale("en");
-//Calendar.momentLocalizer(moment);
+const localizer = BigCalendar.momentLocalizer(moment);
+const myEventsList = eventsList;
+ 
 
-class CalenderApp extends Component {
-    state = {
-      view: "day",
-      date: new Date(2015, 3, 12),
-      width: 500
-    };
-  
-    componentDidMount() {
-      window.addEventListener("resize", () => {
-      
-      });
-    }
-  
-    render() {
-      return (
-        <div style={{ height: 700 }}>
-          <button onClick={() => this.setState({ view: "day" })}>Day</button>
-          <button onClick={() => this.setState({ view: "month" })}>Month</button>
-          <Calendar
-            style={{ height: 500, width: this.state.width }}
-            toolbar={false}
-            events={events}
-            step={60}
-            //views={allViews}
-            view={this.state.view}
-            onView={() => {}}
-            date={this.state.date}
-            onNavigate={date => this.setState({ date })}
-          />
-        </div>
-      );
-    }
-  }
-  
-  export default CalenderApp;
+function CalenderApp() {
+  return (
+    <div className="App">
+      <BigCalendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+      />
+    </div>
+  );
+}
+
+export default CalenderApp;
