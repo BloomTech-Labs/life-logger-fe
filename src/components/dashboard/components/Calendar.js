3d9fc35a24a12aec48b3196aded34d9030228f55
Carl-Stanley <cds.beml@gmail.com>
2020-03-26 12:09:56 -0700

    Removing unneeded DIV

--- src/components/dashboard/components/Calendar.js
+++ src/components/dashboard/components/Calendar.js
@@ -23,9 +23,7 @@ export default class CalendarApp extends Component {
     render() {
       return (
         <div className="calendar-app" style={{ height: 600, width: 800, marginTop:100 }}>
-          <div className="calendar-app-top">
-        
-          </div>
+          
           <div className="calendar-app-calendar">
             <FullCalendar
               defaultView="dayGridMonth"
