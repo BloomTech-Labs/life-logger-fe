ed7c13286742a7b4575f68f67936658cc36096a9
Carl-Stanley <cds.beml@gmail.com>
2020-03-30 16:03:25 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@
 import './App.css';
 
 //Packages
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-import { Route } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
-import Dashboard from "./components/dashboard";
-import EditTask from "./components/dashboard/components/EditTask";
-import Task from "./components/dashboard/components/Task";
-import Header from "./components/landing-page/components/Header";
-import { AppContainer } from "./styles/Styles";
-import GAinit, { initGA } from './InitGA'
-import CalendarApp from "./components/dashboard/components/Calendarlist";
-=======
 import React, { useEffect } from 'react';
 import { initGA } from './InitGA';
 import PrivateRoute from './PrivateRoute';
@@ -23,7 +11,6 @@ import EditTask from './components/dashboard/components/EditTask';
 import Task from './components/dashboard/components/Task';
 import Header from './components/landing-page/components/Header';
 import { AppContainer } from './styles/Styles';
->>>>>>> 075bb5cc718e8d974893155268565b8fdbf0659b
 
 function App() {
   useEffect(() => {
--- src/components/dashboard/components/Calendar.js
+++ src/components/dashboard/components/Calendar.js
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-import { useHistory } from 'react-router-dom';
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction"; 
-import "@fullcalendar/core/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
-import moment from 'moment-timezone';
-//import eventsList from "./TestData";
-
-const Calendaritems = props => {
-  
-  const history = useHistory(); 
-  
-    // Grab All of the events for the logged in user.  
-    const theEvents = props.events.map(AllEvents => { return{id: AllEvents.id,title: AllEvents.title,start: AllEvents.event_st_tm,end: AllEvents.event_et_tm };
-           
-    }); 
-    
-    //Update an event
-    const handleEventClick = eventID => {
-
-      console.log("AllEvents:" + theEvents);
-      
-      console.log("event id: " + eventID)
-          if (window.confirm("Would you like to modify this event?")) {
-        history.push(`/task/${eventID}`); 
-    }};
-    
-    // Add a new event
-    const handleDateClick = arg => {
-      if (window.confirm("Would you like to add an event to " + moment(arg.dateStr).format('MM/DD/YYYY') + " ?")) {    
-        
-        history.push(`/`);
-
-      }
-    }
-    
-    return (           
-          
-          <div className="calendar-app" style={{ marginTop:100, marginBottom:50 }}>
-          
-            <div className="calendar-app-calendar">
-              <FullCalendar
-                height= {600}
-                width = {700} 
-                defaultView="dayGridMonth"
-                editable= {true}
-                header={{
-                  left: "prev,next",
-                  center: "title",
-                  right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
-                  
-                }}
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                events={theEvents} 
-                eventClick={() => handleEventClick(66)}
-                dateClick={handleDateClick}
-                //eventClick={() => handleEventClick(theEvents.id)}          
-              />
-            </div>
-          </div>          
-        
-      );
-};
-
-export default Calendaritems;
-=======
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
@@ -148,4 +77,3 @@ const Calendar = () => {
 };
 
 export default Calendar;
->>>>>>> 075bb5cc718e8d974893155268565b8fdbf0659b
