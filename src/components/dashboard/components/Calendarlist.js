e526a82dd3f2aaed82a95815e13c9541dd608f1c
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-30 11:22:37 -0400

    pulls user id from localstorage

--- src/components/dashboard/components/Calendarlist.js
+++ src/components/dashboard/components/Calendarlist.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchEventsByUserId } from '../../../store/actions';
-import CalendarApp from './Calendar';
 import { Container } from '../../dashboard/styles';
+import CalendarApp from './Calendar';
 
 const Calendarlist = () => {
   const dispatch = useDispatch();
-  const { userData } = useSelector(state => state.users);
+  const tempUserDataId = localStorage.getItem('id');
+  // const { userData } = useSelector(state => state.users);
   const { eventData } = useSelector(state => state.events);
 
   useEffect(
     () => {
-      dispatch(fetchEventsByUserId(userData.user_id));
+      dispatch(fetchEventsByUserId(tempUserDataId));
     },
-    [userData, dispatch]
+    [tempUserDataId, dispatch]
   );
 
   return (
     <Container>
-            <CalendarApp events={eventData} />
+      <CalendarApp events={eventData} />
     </Container>
   );
 };
