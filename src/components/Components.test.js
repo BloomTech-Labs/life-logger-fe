2bd071dab2fcae027555384c92170dde75542b05
Carl-Stanley <cds.beml@gmail.com>
2020-04-20 19:47:52 -0700

    Commit adding addtional tests

--- src/components/Components.test.js
+++ src/components/Components.test.js
@@ -5,6 +5,9 @@ import Task from './dashboard/components/Task';
 import TaskList from './dashboard/components/TaskList'; 
 import Dashboard from './dashboard/index';
 import { BrowserRouter} from 'react-router-dom';
+import CalendarApp from './dashboard/components/Calendar';
+import CalendarItems from './dashboard/components/Calendarlist';
+
 
 // EditTask render test
 it('renders without crashing', ()=> {
@@ -43,5 +46,21 @@ it('renders without crashing', ()=> {
     <BrowserRouter>
     const div = document.createElement('div');
     ReactDom.render(<Dashboard />, div);
+    </BrowserRouter>
+})
+
+// CalendarApp render test
+it('renders without crashing', ()=> {
+    <BrowserRouter>
+    const div = document.createElement('div');
+    ReactDom.render(<CalendarApp />, div);
+    </BrowserRouter>
+})
+
+// CalendarItems render test
+it('renders without crashing', ()=> {
+    <BrowserRouter>
+    const div = document.createElement('div');
+    ReactDom.render(<CalendarItems />, div);
     </BrowserRouter>
 })
