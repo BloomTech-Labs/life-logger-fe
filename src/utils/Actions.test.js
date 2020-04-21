67b1eec56496e2d3f3a339148f6442d3dfb4375f
Carl-Stanley <cds.beml@gmail.com>
2020-04-20 20:20:06 -0700

    Commit

---
+++ src/utils/Actions.test.js
@@ -0,0 +1,17 @@
+import createAStore from './testStore.js';
+
+describe('dataloader Actions', () => {
+    const expectedState = {
+      title: 'Example title'
+    };
+
+
+    const store =  createAStore();
+    let newState = store.getState();
+
+        
+
+
+
+
+});
