7d556ea9ee65cea71198e3b2578eb28df72fdc32
sagdish <forsi1@yahoo.com>
2020-04-22 19:10:45 -0700

    remove service worker line

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import { applyMiddleware, compose, createStore } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./store/reducers";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -29,8 +28,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
