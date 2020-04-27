9e465676253869985e4251520e09ee049fbabeb4
sagdish <forsi1@yahoo.com>
2020-04-27 16:00:57 -0700

    settings page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { initGA } from './InitGA';
 import PrivateRoute from './PrivateRoute';
 import Dashboard from './components/dashboard';
 import Calendar from './components/dashboard/components/Calendar';
+import Settings from './components/dashboard/components/Settings';
 import EditTask from './components/dashboard/components/EditTask';
 import Task from './components/dashboard/components/Task';
 import Header from './components/landing-page/components/Header';
@@ -24,6 +25,7 @@ function App() {
       <PrivateRoute path="/task/:id" component={Task} />
       <PrivateRoute path="/edit-task/:id" component={EditTask} />
       <PrivateRoute path="/calendar" component={Calendar} />
+      <PrivateRoute path="/settings" component={Settings} />
     </AppContainer>
   );
 }
