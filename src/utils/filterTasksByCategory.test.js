e86564604f992ed909c6ddd5d8ae0bdd2aad2b67
Janessa Garrow <janessa.garrow@gmail.com>
2020-05-20 19:40:59 -0600

    Add test for filter when sortStatus is not 3

--- src/utils/filterTasksByCategory.test.js
+++ src/utils/filterTasksByCategory.test.js
@@ -2,48 +2,57 @@ import expect from 'expect';
 
 import { filterTasksByCategory } from './filterTasksByCategory';
 
+const tasks = [
+    {
+        category: 1,
+        event_et_tm: '2020-06-06T17:45:00.000Z',
+        event_st_tm: '2020-06-05T17:11:00.000Z',
+    },
+    {
+        category: 0,
+        event_et_tm: '2020-05-16T17:45:00.000Z',
+        event_st_tm: '2020-05-16T17:11:00.000Z',
+    },
+    {
+        category: 0,
+        event_et_tm: '2020-05-19T17:45:00.000Z',
+        event_st_tm: '2020-05-19T17:11:00.000Z',
+    },
+    {
+        category: 2,
+        event_et_tm: '2020-06-02T17:45:00.000Z',
+        event_st_tm: '2020-06-02T17:11:00.000Z',
+    },
+    {
+        category: 1,
+        event_et_tm: '2020-05-18T17:45:00.000Z',
+        event_st_tm: '2020-05-18T17:11:00.000Z',
+    },
+    {
+        category: 2,
+        event_et_tm: '2020-05-19T17:45:00.000Z',
+        event_st_tm: '2020-05-19T17:11:00.000Z',
+    },
+    {
+        category: 1,
+        event_et_tm: '2020-06-02T17:45:00.000Z',
+        event_st_tm: '2020-06-02T17:11:00.000Z',
+    },
+];
+
 describe('filterTasksByCategory', () => {
     it('successfully shows all tasks regardless of date', () => {
-        const tasks = [
-            {
-                category: 1,
-                event_et_tm: '2020-06-06T17:45:00.000Z',
-                event_st_tm: '2020-06-05T17:11:00.000Z',
-            },
-            {
-                category: 0,
-                event_et_tm: '2020-05-16T17:45:00.000Z',
-                event_st_tm: '2020-05-16T17:11:00.000Z',
-            },
-            {
-                category: 0,
-                event_et_tm: '2020-05-19T17:45:00.000Z',
-                event_st_tm: '2020-05-19T17:11:00.000Z',
-            },
-            {
-                category: 2,
-                event_et_tm: '2020-06-02T17:45:00.000Z',
-                event_st_tm: '2020-06-02T17:11:00.000Z',
-            },
-            {
-                category: 1,
-                event_et_tm: '2020-05-18T17:45:00.000Z',
-                event_st_tm: '2020-05-18T17:11:00.000Z',
-            },
-            {
-                category: 2,
-                event_et_tm: '2020-05-19T17:45:00.000Z',
-                event_st_tm: '2020-05-19T17:11:00.000Z',
-            },
-            {
-                category: 1,
-                event_et_tm: '2020-06-02T17:45:00.000Z',
-                event_st_tm: '2020-06-02T17:11:00.000Z',
-            },
-        ];
-
         const sortStatus = 3; // sortStatus for getting all categories is 3
         const expectedLength = tasks.length;
+
+        const actual = filterTasksByCategory(tasks, sortStatus);
+
+        expect(actual.length).toBe(expectedLength);
+    });
+
+    it('successfully shows tasks with the same category as the sortStatus', () => {
+        const sortStatus = 2;
+        const expectedLength = 2;
 
         const actual = filterTasksByCategory(tasks, sortStatus);
 
