12e798782b7497f3f02259c16f105b59503f47d7
Janessa Garrow <janessa.garrow@gmail.com>
2020-06-03 00:14:10 -0600

    Fix issues with mocking redux store. Add another test to check if correct task title is displayed after successful render of Task component

--- src/components/dashboard/components/__tests__/Task.test.js
+++ src/components/dashboard/components/__tests__/Task.test.js
@@ -1,38 +1,87 @@
 import { renderWithRouter } from '../../../../../tests/routerTestsUtil';
-import configureStore from 'redux-mock-store';
+import { within } from '@testing-library/react';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import * as actions from '../../../../store/actions/index';
+import rootReducer from '../../../../store/reducers';
 
 import Task from '../Task';
 
-const mockStore = configureStore([]);
+jest.mock('../../../../store/actions/index');
+
+// mock the fetchEvent action creator -- we don't need to test it here since there are already unit tests written for the Actions file
+actions.fetchEvent.mockImplementation(() => (dispatch) => {
+  return dispatch({
+    type: 'FETCH_EVENT_SUCCESS',
+    payload: {
+      id: 180,
+      title: 'My task',
+    },
+  });
+});
 
 describe('Task component', () => {
   let store;
+  const initialState = {
+    events: {
+      eventData: [
+        {
+          id: 180,
+          title: 'My task',
+        },
+        {
+          id: 181,
+          title: 'My second task',
+        },
+      ],
+      currentEvent: null,
+    },
+  };
 
   beforeEach(() => {
-    store = mockStore({
-      events: {
-        eventData: [
-          {
-            id: 180,
-            title: 'My task',
-          },
-          {
-            id: 181,
-            title: 'My second task',
-          },
-        ],
-      },
-    });
+    store = createStore(rootReducer, initialState, applyMiddleware(thunk));
   });
 
   it('renders without crashing', () => {
     renderWithRouter(
       Task,
-      { store },
       {
-        path: '/task/180',
-        route: '/task/:id',
+        initialState,
+        store,
+      },
+      {
+        route: '/task/180',
+        path: '/task/:id',
       }
     );
+  });
+
+  it('displays the correct task title', async () => {
+    // mock fetchEvent, hard code the payload as the event we want to set as the `currentEvent`
+    actions.fetchEvent.mockImplementation(() => (dispatch) => {
+      return dispatch({
+        type: 'FETCH_EVENT_SUCCESS',
+        payload: {
+          id: 180,
+          title: 'My task',
+        },
+      });
+    });
+
+    const { findByTestId } = renderWithRouter(
+      Task,
+      {
+        initialState,
+        store,
+      },
+      {
+        route: '/task/180',
+        path: '/task/:id',
+      }
+    );
+
+    const actual = await findByTestId('taskTitle');
+
+    within(actual).getByText('My task');
   });
 });
