642ffbaf88115ec985e5a8f9fba89a3285da894c
Janessa Garrow <janessa.garrow@gmail.com>
2020-06-13 14:45:56 -0600

    Create env variable for backend host

--- src/utils/isAuthenticated.js
+++ src/utils/isAuthenticated.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const isAuthenticated = async (token) => {
   try {
     const res = await axios.post(
-      `http://localhost:5000/api/auth/validate-token`,
+      `${process.env.BACKEND_HOST}/api/auth/validate-token`,
       null,
       {
         headers: {
