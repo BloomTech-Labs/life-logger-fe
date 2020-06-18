53e0aef34ba9fdceeb1ac81547cd7e059b2a4a0c
Wade-Coplen <wadecoplen@gmail.com>
2020-06-18 16:37:06 -0700

    added form control to display errors, touched fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -10,6 +10,7 @@ const initialValues = {
   }
   const validate = values => {
     let errors = {}
+    
     if(!values.username) {
         errors.username = 'Required'
     }
@@ -18,6 +19,7 @@ const initialValues = {
     } else if(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
         errors.password = 'Invalid password'
         return errors;
+        
 }
 
 
@@ -30,25 +32,35 @@ const labelStyle = {
   fontWeight: 'bold',
   display: 'flex',
   marginBottom: '5px',
+  
 };
 const inputStyle = {
   display: 'block',
   width: '400px',
-  padding: '6px 12px',
+  padding: '6 px 12px',
   fontSize: '14px',
-  lineHeight: '1.428573',
+  lineHeight: '1.5',
+  
 };
+const formControl = {
+  marginBottom: '20px'
+}
+const error = {
+  color: 'red'
+}
 export default function LoginForm() {
   const formik = useFormik({
     initialValues,
     onSubmit,
     validate
   })
-    
-  console.log('Form values', formik.values);
+  console.log('form touched', formik.touched)
+  // console.log('form errors', formik.errors)
+  // console.log('Form values', formik.values);
   return (
     <div style={loginFormStyle}>
       <form onSubmit={formik.handleSubmit}>
+        <div className={formControl}>
         <label style={labelStyle} htmlFor="username">
           Username:
         </label>
@@ -58,8 +70,12 @@ export default function LoginForm() {
           id="username"
           maxLength="25"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.username}
         />
+        {formik.touched.username && formik.errors.username ? <div className='error'>{formik.errors.username}</div> : null}
+        </div>
+        <div className={formControl}>
         <label style={labelStyle} htmlFor="password">
           Password:
         </label>
@@ -69,8 +85,11 @@ export default function LoginForm() {
           id="password"
           minLength="8"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.password}
         />
+        {formik.touched.password && formik.errors.password ? <div className='error'>{formik.errors.username}</div> : null}
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
