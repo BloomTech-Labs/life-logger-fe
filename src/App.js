d5b17d4ac0a134db55dc38b8a080c628fb112f37
Devin Dias <devindias.tech@gmail.com>
2020-06-22 20:40:10 -0500

    adds .vscode to gitignore

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-
+import taskContext from "./context/task_context";
 import Header from './components/header/Header';
 import PrivateRoute from './PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
+import axios from "axios"
+
+
 
 const App = () => {
+  const fakedata = [1,2,3]
+
+
   return (
+    <taskContext.Provider value={{ fakedata }}>
     <div>
       <Header />
       <h1 sx={{ textAlign: `center` }}>Welcome to Lyfe Logger</h1>
 
       <PrivateRoute exact path="/dashboard" component={Dashboard} />
     </div>
+    </taskContext.Provider>
   );
 };
 
