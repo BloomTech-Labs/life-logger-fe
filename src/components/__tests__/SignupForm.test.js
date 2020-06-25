a593282d9d397b50743b994017cc77d968e8cf61
Janessa Garrow <janessa.garrow@gmail.com>
2020-06-24 22:57:16 -0600

    Fix SignupForm test

--- src/components/__tests__/SignupForm.test.js
+++ src/components/__tests__/SignupForm.test.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import { render } from '../../../tests/themeProviderTestsUtil';
-import taskContext from '../../context/task_context';
-
+import { renderWithRouter } from '../../../tests/routerTestsUtil';
 import SignupForm from '../SignupForm';
-
-const SignUp = () => {
-
-}
 
 describe('SignupForm component tests', () => {
   it('renders without crashing', () => {
-    render(<taskContext.Provider value={{ SignUp }}><SignupForm /></taskContext.Provider>);
+    renderWithRouter(SignupForm);
   });
 });
