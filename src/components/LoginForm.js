f3b5f0e3b598bfec8745e116915a95d1218d3c5b
Janessa Garrow <janessa.garrow@gmail.com>
2020-06-25 10:08:51 -0600

    Update onSubmit function to hit backend endpoint. Cleanup styling to match SignupForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,76 +1,45 @@
 /**@jsx jsx*/
-import {jsx} from'theme-ui';
-import {Input, Label, Button} from '@theme-ui/components'
-import React from 'react';
+import { jsx } from 'theme-ui';
+import { Input, Label, Button } from '@theme-ui/components';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
+import ReactRouterPropTypes from 'react-router-prop-types';
 
-const initialValues = {
-    username: '',
-    password: ''    
-  }
-  const onSubmit = value => {
-      console.log("Form data", value)
-  }
-  const validate = values => {
-    let errors = {}
-    
-    if(!values.username) {
-        errors.username = 'Required'
-    }
-    if(!values.password) {
-        errors.password = 'Required'
-    }
-        return errors;
-        
-}
 const validationSchema = Yup.object({
-  username: Yup.string()
-  .required('Required'),
-  password: Yup.string()
-  .required('Required')
-  
-})
-//Styling
-const loginFormStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-};
-const labelStyle = {
-  fontWeight: 'bold',
-  display: 'flex',
-  marginBottom: '5px',
-  
-};
-const inputStyle = {
-  display: 'block',
-  width: '265px',
-  padding: '6 px 12px',
-  fontSize: '14px',
-  lineHeight: '1.5',
-  
-};
-const formControl = {
-  marginBottom: '20px'
-}
-const error = {
-  color: 'red'
-}
+  username: Yup.string().required('Required'),
+  password: Yup.string().required('Required'),
+});
 
-export default function LoginForm() {
+export default function LoginForm(props) {
+  const initialValues = {
+    username: '',
+    password: '',
+  };
+
+  const onSubmit = (value) => {
+    return axios
+      .post('https://lyfe-logger-be.herokuapp.com/api/auth/login', value)
+      .then((res) => {
+        window.localStorage.setItem('token', res.data.token);
+        window.localStorage.setItem('userId', res.data.user_id); // needed for TaskContext to make axios requests for tasks
+        props.history.push('/dashboard');
+      })
+      .catch((err) => {
+        console.log('Error logging in: ', err);
+        props.history.push('/login');
+      });
+  };
+
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validationSchema
-    // validate
-  })
-  // console.log('form touched', formik.touched)
-  // console.log('form errors', formik.errors)
-  // console.log('Form values', formik.values);
+    validationSchema,
+  });
   return (
-    <div sx={loginFormStyle}>
-      <form onSubmit={formik.handleSubmit}
-       sx={{
+    <form
+      onSubmit={formik.handleSubmit}
+      sx={{
         width: `300px`,
         margin: `0 auto`,
         display: `grid`,
@@ -79,46 +48,49 @@ export default function LoginForm() {
         borderRadius: `12px`,
         bg: (t) => t.colors.primary,
         boxShadow: `0 3px 3px 0 rgba(0, 0, 0, 0.16), 0 3px 3px 0 rgba(0, 0, 0, 0.23)`,
-      }} >
-        <div className={formControl}>
-        <Label sx={labelStyle} htmlFor="username">
-          Username:
-        </Label>
-        <Input
-          sx={inputStyle}
-          type="text"
-          id="username"
-          name='username'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.username}
-        />
-        {formik.touched.username && formik.errors.username ? <div className='error'>{formik.errors.username}</div> : null}
-        </div>
-
-        <div className={formControl}>
-        <Label sx={labelStyle} htmlFor="password">
-          Password:
-        </Label>
-        <Input
-          sx={inputStyle}
-          type="password"
-          id="password"
-          name='password'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-        {formik.touched.password && formik.errors.password ? <div className='error'>{formik.errors.username}</div> : null}
-        </div>
-        <Button type="submit"    
-                sx={{
-              //   bg: (t) => t.colors.muted,
-              //   color: (t) => t.colors.text,
-                  margin: `0 auto`,
-            }}>Submit</Button>
-      </form>
-    </div>
+      }}
+    >
+      <Label htmlFor="username">Username</Label>
+      <Input
+        type="text"
+        id="username"
+        name="username"
+        mb={3}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.username}
+      />
+      {formik.touched.username && formik.errors.username ? (
+        <div className="error">{formik.errors.username}</div>
+      ) : null}
+      <Label htmlFor="password">Password</Label>
+      <Input
+        type="password"
+        id="password"
+        name="password"
+        mb={3}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <div className="error">{formik.errors.username}</div>
+      ) : null}
+      <Button
+        type="submit"
+        sx={{
+          margin: `0 auto`,
+        }}
+      >
+        Submit
+      </Button>
+    </form>
   );
 }
 
+LoginForm.propTypes = {
+  history: ReactRouterPropTypes.history,
+  location: ReactRouterPropTypes.location,
+  match: ReactRouterPropTypes.match,
+  route: ReactRouterPropTypes.route,
+};
