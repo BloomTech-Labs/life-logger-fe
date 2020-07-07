f5d4b66a8e0eab95de542f965e936867c2481abb
Wade-Coplen <wadecoplen@gmail.com>
2020-07-06 17:59:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import { Fragment } from 'react';
 import SignUpForm from './components/SignupForm';
 import PrivateRoute from './PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
-import { Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import LoginForm from './components/LoginForm';
+import TaskPage from './components/TaskPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <Fragment>
-      <Route exact path="/signup" component={SignUpForm} />
-      <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      <Router>
+        <Switch>
+          <Route path="/LandingPage" component={LandingPage} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <Route path="/" component={SignUpForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/Taskpage" component={TaskPage} />
+        </Switch>
+      </Router>
     </Fragment>
   );
 };
