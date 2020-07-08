f1653548d8f1adcda5bc974fd93b78b7367ecfdf
Wade-Coplen <wadecoplen@gmail.com>
2020-07-07 21:00:58 -0700

    fixying husky errors

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,19 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
     <div>
       <h1>Welcome to</h1>
       <img src="OrangeLogo.png" alt="orange squares icon" />
-      <button>Sign Up!</button>
-      <button>Sign In!</button>
+
+      <Link to="/SignUp">
+        <button>Sign Up!</button>
+      </Link>
+      <Link to="/Login">
+        <button>Sign In!</button>
+      </Link>
     </div>
   );
 };
