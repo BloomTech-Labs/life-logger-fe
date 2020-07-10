c2fe1aa429f6aee201ad0074781939a6f4c53fb5
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-09 20:46:51 -0600

    Write test to check if TaskCheckmark renders

---
+++ src/components/dashboard/__tests__/TaskCheckmark.test.js
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { render } from '../../../../tests/themeProviderTestsUtil';
+
+import TaskCheckmark from '../TaskCheckmark';
+
+describe('TaskCheckmark tests', () => {
+  it('renders without crashing', () => {
+    const toggleComplete = jest.fn();
+    const isChecked = false;
+    const handleKeyEnterPress = jest.fn();
+
+    render(
+      <TaskCheckmark
+        toggleComplete={toggleComplete}
+        isChecked={isChecked}
+        handleKeyEnterPress={handleKeyEnterPress}
+      />
+    );
+  });
+});
