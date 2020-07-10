af1b4a9bfc8893e6e02599f9022d4028f5dfb23b
davidmoates <david.m.moates@gmail.com>
2020-07-09 23:09:56 -0500

    I created the form for Creating a Task

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { jsx } from 'theme-ui';
 import { Fragment } from 'react';
 import SignUpForm from './components/SignupForm';
+import CreateTask from './components/CreateTask'
 import PrivateRoute from './PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
 import { Route } from 'react-router-dom';
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <Fragment>
       <Route exact path="/signup" component={SignUpForm} />
+      <Route exact path="/createTask" component={CreateTask} />
       <PrivateRoute exact path="/dashboard" component={Dashboard} />
     </Fragment>
   );
