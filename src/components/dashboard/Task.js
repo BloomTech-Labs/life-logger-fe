5f3a2fa910573fd39a1697aa62bbe1253481bc6c
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-12 14:29:53 -0600

    Update strikethrough animations to animate when marked complete or incomplete

--- src/components/dashboard/Task.js
+++ src/components/dashboard/Task.js
@@ -11,10 +11,12 @@ const pStyles = {
 
 const Task = ({ task }) => {
   const [isComplete, setIsComplete] = useState(task.is_complete);
+  const [isNotInitial, setIsNotInitial] = useState(false); // state to prevent "unstrike" keyframe animation from running on mount
 
   const taskDueDateObj = new Date(task.due_date);
   const dueDate = taskDueDateObj.toLocaleDateString();
 
+  // adapted from https://codepen.io/EdenSG/pen/jPENoE
   // any text that you want to be crossed out with an animation
   // inside of your p tag, wrap the text in a span tag
   // and give the span these stylings to be triggered
@@ -27,30 +29,54 @@ const Task = ({ task }) => {
     '&:after': {
       content: `''`,
       position: `absolute`,
-      display: isComplete ? `block` : `none`,
+      display: `block`,
+      opacity: `0`,
       width: `100%`,
       height: `2px`,
       boxShadow: `0 1px rgba(255,255,255,0.6)`,
       marginTop: `-0.7em`,
       background: `#9B9B9B`,
-      transformOrigin: `center left`,
-      animation: `strikethrough 0.5s cubic-bezier(.55, 0, .1, 1) 1 forwards`,
+      transformOrigin: isComplete ? `center left` : `center right`,
+      animation: isComplete
+        ? `strikethrough 0.5s cubic-bezier(.55, 0, .1, 1) 1 forwards`
+        : `unstrike 0.5s cubic-bezier(.55, 0, .1, 1) 1 forwards`,
       transform: `scaleX(0)`,
       transition: `transform 0.5s cubic-bezier(.55, 0, .1, 1)`,
     },
 
     '@keyframes strikethrough': {
-      from: {
+      '0%': {
         transform: `scaleX(0)`,
+        opacity: `0`,
       },
-      to: {
+      '25%': {
+        opacity: `1`,
+      },
+      '100%': {
         transform: `scaleX(1)`,
+        opacity: `1`,
+      },
+    },
+
+    '@keyframes unstrike': {
+      '0%': {
+        transform: `scaleX(1)`,
+        opacity: isNotInitial ? `1` : `0`, // kind of hacky, but prevents unstrike animation from being "visible" when component mounts
+      },
+      '75%': {
+        opacity: isNotInitial ? `1` : `0`, // kind of hacky, but prevents unstrike animation from being "visible" when component mounts
+      },
+      '100%': {
+        transform: `scaleX(0)`,
+        opacity: `0`,
       },
     },
   };
 
   const toggleComplete = () => {
     setIsComplete(!isComplete);
+    setIsNotInitial(true); // allows "unstrike" animation to run now whenever a task is marked incomplete
+
     // make PUT request to backend to update `is_complete` for this task
   };
 
