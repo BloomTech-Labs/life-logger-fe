e2a606f3ebb6fafff6271ea2e238afbca21ccf0c
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-12 14:31:56 -0600

    Update dummy task data structure to match updated backend

--- src/components/dashboard/__tests__/Task.test.js
+++ src/components/dashboard/__tests__/Task.test.js
@@ -8,16 +8,11 @@ describe('Task tests', () => {
   it('renders without crashing', () => {
     const task = {
       all_day: true,
-      category: 1,
-      event_ct_tm: '2020-05-28T03:36:47.000Z',
-      event_et_tm: '2020-05-11T17:00:00.000Z',
-      event_resource: '',
-      event_st_tm: '2020-05-11T16:00:00.000Z',
-      event_text: 'Here are some notes',
+      due_date: '2020-05-28T03:36:47.000Z',
+      task_notes: 'Here are some notes',
       id: 178,
-      iscomplete: false,
-      location: 'Home',
-      title: 'A task',
+      is_complete: false,
+      name: 'A task',
       user_id: 49,
     };
 
