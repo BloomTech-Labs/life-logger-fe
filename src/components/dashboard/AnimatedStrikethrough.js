21bb9fff9d8ce1639211e6438f0b50c856997408
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-13 13:10:09 -0600

    Move animated strike through element out to own component

---
+++ src/components/dashboard/AnimatedStrikethrough.js
@@ -0,0 +1,75 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import PropTypes from 'prop-types';
+
+const AnimatedStrikethrough = ({
+  stringToStrike,
+  isStruckOut,
+  isNotInitial,
+}) => {
+  return (
+    <span
+      sx={{
+        display: `inline-block`,
+        position: `relative`,
+        transition: `all 0.5s cubic-bezier(.55, 0, .1, 1)`,
+
+        '&:after': {
+          content: `''`,
+          position: `absolute`,
+          display: `block`,
+          opacity: `0`,
+          width: `100%`,
+          height: `2px`,
+          boxShadow: `0 1px rgba(255,255,255,0.6)`,
+          marginTop: `-0.7em`,
+          background: `#9B9B9B`,
+          transformOrigin: isStruckOut ? `center left` : `center right`,
+          animation: isStruckOut
+            ? `strikethrough 0.5s cubic-bezier(.55, 0, .1, 1) 1 forwards`
+            : `unstrike 0.5s cubic-bezier(.55, 0, .1, 1) 1 forwards`,
+          transform: `scaleX(0)`,
+          transition: `transform 0.5s cubic-bezier(.55, 0, .1, 1)`,
+        },
+
+        '@keyframes strikethrough': {
+          '0%': {
+            transform: `scaleX(0)`,
+            opacity: `0`,
+          },
+          '25%': {
+            opacity: `1`,
+          },
+          '100%': {
+            transform: `scaleX(1)`,
+            opacity: `1`,
+          },
+        },
+
+        '@keyframes unstrike': {
+          '0%': {
+            transform: `scaleX(1)`,
+            opacity: isNotInitial ? `1` : `0`, // kind of hacky, but prevents unstrike animation from being "visible" when component mounts
+          },
+          '75%': {
+            opacity: isNotInitial ? `1` : `0`, // kind of hacky, but prevents unstrike animation from being "visible" when component mounts
+          },
+          '100%': {
+            transform: `scaleX(0)`,
+            opacity: `0`,
+          },
+        },
+      }}
+    >
+      {stringToStrike}
+    </span>
+  );
+};
+
+AnimatedStrikethrough.propTypes = {
+  stringToStrike: PropTypes.string,
+  isStruckOut: PropTypes.bool,
+  isNotInitial: PropTypes.bool,
+};
+
+export default AnimatedStrikethrough;
--- src/components/dashboard/Task.js
+++ src/components/dashboard/Task.js
@@ -1,83 +1,32 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import { useState } from 'react';
-import TaskCheckmark from './TaskCheckmark';
+import { useState, useContext } from 'react';
+import TaskContext from '../../context/TaskContext';
 import PropTypes from 'prop-types';
+
+import TaskCheckmark from './TaskCheckmark';
 import Card from './Card';
+import AnimatedStrikethrough from './AnimatedStrikethrough';
 
 const pStyles = {
   margin: `0`,
 };
 
 const Task = ({ task }) => {
+  const { updateTask } = useContext(TaskContext);
   const [isComplete, setIsComplete] = useState(task.is_complete);
   const [isNotInitial, setIsNotInitial] = useState(false); // state to prevent "unstrike" keyframe animation from running on mount
 
   const taskDueDateObj = new Date(task.due_date);
   const dueDate = taskDueDateObj.toLocaleDateString();
 
-  // adapted from https://codepen.io/EdenSG/pen/jPENoE
-  // any text that you want to be crossed out with an animation
-  // inside of your p tag, wrap the text in a span tag
-  // and give the span these stylings to be triggered
-  // when `isComplete` is toggled
-  const strikeThroughStyles = {
-    display: `inline-block`,
-    position: `relative`,
-    transition: `all 0.5s cubic-bezier(.55, 0, .1, 1)`,
-
-    '&:after': {
-      content: `''`,
-      position: `absolute`,
-      display: `block`,
-      opacity: `0`,
-      width: `100%`,
-      height: `2px`,
-      boxShadow: `0 1px rgba(255,255,255,0.6)`,
-      marginTop: `-0.7em`,
-      background: `#9B9B9B`,
-      transformOrigin: isComplete ? `center left` : `center right`,
-      animation: isComplete
-        ? `strikethrough 0.5s cubic-bezier(.55, 0, .1, 1) 1 forwards`
-        : `unstrike 0.5s cubic-bezier(.55, 0, .1, 1) 1 forwards`,
-      transform: `scaleX(0)`,
-      transition: `transform 0.5s cubic-bezier(.55, 0, .1, 1)`,
-    },
-
-    '@keyframes strikethrough': {
-      '0%': {
-        transform: `scaleX(0)`,
-        opacity: `0`,
-      },
-      '25%': {
-        opacity: `1`,
-      },
-      '100%': {
-        transform: `scaleX(1)`,
-        opacity: `1`,
-      },
-    },
-
-    '@keyframes unstrike': {
-      '0%': {
-        transform: `scaleX(1)`,
-        opacity: isNotInitial ? `1` : `0`, // kind of hacky, but prevents unstrike animation from being "visible" when component mounts
-      },
-      '75%': {
-        opacity: isNotInitial ? `1` : `0`, // kind of hacky, but prevents unstrike animation from being "visible" when component mounts
-      },
-      '100%': {
-        transform: `scaleX(0)`,
-        opacity: `0`,
-      },
-    },
-  };
-
   const toggleComplete = () => {
     setIsComplete(!isComplete);
     setIsNotInitial(true); // allows "unstrike" animation to run now whenever a task is marked incomplete
 
     // make PUT request to backend to update `is_complete` for this task
+    // need to pass in task id
+    updateTask({ id: task.id, is_complete: isComplete });
   };
 
   // for accessibility for "hidden" checkbox in `TaskCheckmark`
@@ -123,9 +72,13 @@ const Task = ({ task }) => {
                 : 'none',
             }}
           >
-            <span sx={strikeThroughStyles}>I am a task</span>
+            <AnimatedStrikethrough
+              stringToStrike="I am a task"
+              isStruckOut={isComplete}
+              isNotInitial={isNotInitial}
+            />
+            {/* <AnimatedStrikethrough stringToStrike={task.name} isStruckOut={isComplete} isNotInitial={isNotInitial}/> */}
           </p>
-          {/* <p sx={{ ...pStyles, fontWeight: 700 }}>{task.task_name}</p> */}
           <p sx={pStyles}>{'>'}</p>
         </div>
 
@@ -135,7 +88,11 @@ const Task = ({ task }) => {
             color: `#9B9B9B`,
           }}
         >
-          <span sx={strikeThroughStyles}>{dueDate}</span>
+          <AnimatedStrikethrough
+            stringToStrike={dueDate}
+            isStruckOut={isComplete}
+            isNotInitial={isNotInitial}
+          />
         </p>
       </div>
     </Card>
