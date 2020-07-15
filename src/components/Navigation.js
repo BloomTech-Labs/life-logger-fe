9afbc596414eaa77fc20beb985a1e3c5f972861f
Jackie Atwood <takalaj@verizon.net>
2020-07-14 20:41:26 -0500

    hamburger and header test

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,46 +1,34 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import { NavLink } from 'theme-ui';
-import { Flex } from "theme-ui";
 import styled from '@emotion/styled';
 import DarkModeToggle from './header/DarkModeToggle';
+import BurgerMenu from "./BurgerMenu";
 
 const HeaderGrid = styled.header`
   ${({ theme: t }) => `
     width: 100%;
-    height: 70px;
+    height: 55px;
     padding: 0 2rem;
-    display: grid;
-    grid-template-columns: minmax(40px, 225px) 1fr 40px minmax(100px, 200px);
-    grid-gap: 15px; 
+    display: flex;
+    justify-content: flex-end;
     align-items: center;
     background: ${t.colors.primary};
   `}
 `;
 
-const StyledLink = styled(NavLink)`
-  ${({ theme: t }) => `
-  text-decoration: none;
-  color: ${t.colors.text};
-  `}
-`;
+// const StyledLink = styled(NavLink)`
+//   ${({ theme: t }) => `
+//   text-decoration: none;
+//   color: ${t.colors.text};
+//   `}
+// `;
 
 const Navigation = () => {
     return (
         <HeaderGrid>
-      <DarkModeToggle />
-
-            <Flex as='nav'>
-                <StyledLink href='/' p={2} sx={{hover: "white"}}>
-                    Home
-                </StyledLink>
-                <StyledLink href='/dashboard' p={2}>
-                    Dashboard
-                </StyledLink>
-                <StyledLink href='/about' p={2}>
-                    About
-                </StyledLink>
-            </Flex>
+        <DarkModeToggle />
+        <BurgerMenu />
+                
         </HeaderGrid>
     )
 }
