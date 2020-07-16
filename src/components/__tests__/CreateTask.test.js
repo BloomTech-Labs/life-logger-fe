acb340065e0b31fac7312d5c3c1fe185415a14a5
davidmoates <david.m.moates@gmail.com>
2020-07-15 21:38:39 -0500

    Created a second test for Create Task component

--- src/components/__tests__/CreateTask.test.js
+++ src/components/__tests__/CreateTask.test.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import { render } from '../../../tests/contextProviderTestUtil';
 import { renderWithRouter } from '../../../tests/routerTestsUtil';
 import CreateTask from '../CreateTask';
 
 describe('Create Task component tests', () => {
   it('renders without crashing', () => {
     renderWithRouter(CreateTask, { path: '/insertTask', route: '/insertTask' });
+  });
+  it('render all day input Element', () => {
+    render(<input type="checkbox" name="all_day" />);
   });
 });
