a287ca541f83686d5f7d65271f278446d2398714
Wade-Coplen <wadecoplen@gmail.com>
2020-07-15 19:14:04 -0700

    removed Router from App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,18 @@ import Dashboard from './components/dashboard/Dashboard';
 import LandingPage from './components/LandingPage';
 import LoginForm from './components/LoginForm';
 import TaskPage from './components/TaskPage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <PrivateRoute exact path="/dashboard" component={Dashboard} />
-          <Route path="/signup" component={SignUpForm} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/Taskpage" component={TaskPage} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <Route path="/signup" component={SignUpForm} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/Taskpage" component={TaskPage} />
+      </Switch>
     </Fragment>
   );
 };
