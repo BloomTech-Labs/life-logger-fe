14bd1e8f87ee430b6e86e03cf225cd38ad771d70
Jackie Atwood <takalaj@verizon.net>
2020-07-21 22:42:38 -0500

    Corrections to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-
+import { Fragment } from 'react';
 import SignUpForm from './components/SignupForm';
 import PrivateRoute from './PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
-import LandingPage from './components/LandingPage.js';
-import { Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
+import LoginForm from './components/LoginForm';
+import TaskPage from './components/ViewTask';
+import CreateTask from './components/CreateTask';
+import { Route, Switch } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div>
-      <Navigation/>
-      <Route exact path='/' component={LandingPage}/>
-      <Route path='/signupform' component={SignUpForm}/>
-      <PrivateRoute exact path='/dashboard' component={Dashboard}/>
-      <Footer/>
-    </div>
+    <Fragment>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <Route path="/signup" component={SignUpForm} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/viewtask" component={ViewTask} />
+        <Route path="/createtask" component={CreateTask} />
+      </Switch>
+    </Fragment>
   );
 };
-
 export default App;
