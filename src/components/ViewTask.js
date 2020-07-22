83cbe6711192ac19e32c76ba670b87e6c2201caa
Wade-Coplen <wadecoplen@gmail.com>
2020-07-21 22:00:58 -0700

    working on form state

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -1,8 +1,64 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { useFormik } from 'formik';
 
 const ViewTask = () => {
-  return <div></div>;
+  const viewTaskFormStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+  };
+
+  const labelStyle = {
+    fontWeight: 'bold',
+    display: 'flex',
+    marginBottom: '5px',
+  };
+  const inputStyle = {
+    display: 'block',
+    width: '265px',
+    padding: '6 px 12px',
+    fontSize: '14px',
+    lineHeight: '1.5',
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      dueDate: new Date(),
+      description: '',
+    },
+  });
+
+  return (
+    <div style={viewTaskFormStyle}>
+      <form>
+        <label style={labelStyle} htmlFor="Due Date">
+          Due Date
+        </label>
+        <input
+          style={inputStyle}
+          type="date"
+          id="due date"
+          name="dueDate"
+          onChange={formik.handleChange}
+          value={formik.values.dueDate}
+        />
+
+        <label style={labelStyle} htmlFor="Description">
+          Description
+        </label>
+        <input
+          style={inputStyle}
+          type="text"
+          id="description"
+          name="description"
+          onChange={formik.handleChange}
+          values={formik.values.description}
+        />
+
+        <button>Complete Task</button>
+      </form>
+    </div>
+  );
 };
 
 export default ViewTask;
