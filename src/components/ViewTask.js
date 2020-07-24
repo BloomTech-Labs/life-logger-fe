167e91b2df98e6e8e0e8ad04c1a266b584e37e92
Wade-Coplen <wadecoplen@gmail.com>
2020-07-23 18:13:53 -0700

    add validation to input fields

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -38,6 +38,12 @@ const ViewTask = () => {
     fontSize: '14px',
     lineHeight: '1.5',
   };
+  const formControl = {
+    marginBottom: '20px',
+  };
+  const error = {
+    color: 'red',
+  };
 
   const formik = useFormik({
     initialValues,
@@ -47,32 +53,43 @@ const ViewTask = () => {
 
   return (
     <div style={viewTaskFormStyle}>
-      <form>
-        <label style={labelStyle} htmlFor="Due Date">
-          Due Date
-        </label>
-        <input
-          style={inputStyle}
-          type="date"
-          id="due date"
-          name="dueDate"
-          onChange={formik.handleChange}
-          value={formik.values.dueDate}
-        />
+      <form onSubmit={formik.handleSubmit}>
+        <div className={formControl}>
+          <label style={labelStyle} htmlFor="Due Date">
+            Due Date
+          </label>
+          <input
+            style={inputStyle}
+            type="date"
+            id="due date"
+            name="dueDate"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.dueDate}
+          />
+          {formik.touched.dueDate && formik.errors.dueDate ? (
+            <div className={error}>{formik.errors.dueDate}</div>
+          ) : null}
+        </div>
 
-        <label style={labelStyle} htmlFor="Description">
-          Description
-        </label>
-        <input
-          style={inputStyle}
-          type="text"
-          id="description"
-          name="description"
-          onChange={formik.handleChange}
-          values={formik.values.description}
-        />
-
-        <button>Complete Task</button>
+        <div className={formControl}>
+          <label style={labelStyle} htmlFor="Description">
+            Description
+          </label>
+          <input
+            style={inputStyle}
+            type="text"
+            id="description"
+            name="description"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            values={formik.values.description}
+          />
+          {formik.touched.description && formik.errors.description ? (
+            <div className={error}>{formik.errors.description}</div>
+          ) : null}
+        </div>
+        <button type="completed">Complete Task</button>
       </form>
     </div>
   );
