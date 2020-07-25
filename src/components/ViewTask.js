2174ebdf6a3ff71586b6215e8a3901f3148b91b7
Wade-Coplen <wadecoplen@gmail.com>
2020-07-25 15:22:17 -0700

    added react-icons and add settings icon

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -1,5 +1,9 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { IoIosSettings } from "react-icons/io";
+import { GrLink} from "react-icons/gr";
+
+
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
@@ -18,60 +22,84 @@ const validationSchema = Yup.object().shape({
 
 const ViewTask = () => {
   const viewTaskFormStyle = {
-    display: 'flex',
+    display: 'block',
     justifyContent: 'center',
+
   };
 
   const labelStyle = {
     fontWeight: 'bold',
     display: 'flex',
     marginBottom: '5px',
+    marginLeft: '20%'
   };
   const inputStyle = {
-    display: 'block',
     width: '265px',
     padding: '6 px 12px',
     fontSize: '14px',
     lineHeight: '1.5',
+    marginLeft: '20%'
   };
   const formControl = {
     marginBottom: '20px',
+    
   };
   const error = {
     color: 'red',
+    marginLeft: '20%'
   };
+  const completeTaskButton = {
+    marginLeft: '20%'
+  }
+  const settingsIcon = {
+    marginLeft: '80%'
 
-  return (
-    <Formik
-      style={viewTaskFormStyle}
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      <Form>
-        <div className={formControl}>
+  }
+  const grLink = {
+    marginLeft: '20%',
+    marginBottom: '30%'
+  }
+  const divMomma = {
+   
+  }
+
+
+  return (  
+      
+
+    
+    <div style={divMomma}>
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      
+      <Form style={viewTaskFormStyle}>
+        <div style={formControl}>
+          <div style={settingsIcon} >
+            <IoIosSettings />
+          </div>
           <label style={labelStyle} htmlFor="Due Date">
             Due Date
           </label>
           <Field style={inputStyle} type="date" id="due date" name="dueDate" />
-          <ErrorMessage className={error} name="dueDate" />
+          <ErrorMessage style={error} name="dueDate" />
         </div>
+          <br/>
+          
 
-        <div className={formControl}>
+        <div style={formControl}>
           <label style={labelStyle} htmlFor="Description">
             Description
           </label>
-          <Field
-            style={inputStyle}
-            type="text"
-            id="description"
-            name="description"
-          />
-          <ErrorMessage className={error} name="description" />
+          <Field style={inputStyle} type="text" id="description" name="description" />
+          <div style= {error}>
+            <ErrorMessage name="description" />
+          </div>
         </div>
-        <button type="completed">Complete Task</button>
+        <br />
+        <div style= {grLink}><GrLink /> Tag</div>
+        <button style= {completeTaskButton} type="completed">Complete Task</button>
       </Form>
     </Formik>
+    </div>
   );
 };
 
