e9e61e2ddebdd0463720693c98c018c62d328370
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-28 19:41:32 -0600

    Import Footer component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,18 +1,22 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-
+import { Fragment } from 'react';
 import TaskList from './TaskList';
+import Footer from '../Footer';
 
 const Dashboard = () => {
   return (
-    <div
-      sx={{
-        width: `100%`,
-        padding: `0 1rem`,
-      }}
-    >
-      <TaskList />
-    </div>
+    <Fragment>
+      <div
+        sx={{
+          width: `100%`,
+          padding: `0 1rem`,
+        }}
+      >
+        <TaskList />
+      </div>
+      <Footer />
+    </Fragment>
   );
 };
 
