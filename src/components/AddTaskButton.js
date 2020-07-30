2f84ccd810971167c0bd561259b3b89dfd7324bb
Janessa Garrow <janessa.garrow@gmail.com>
2020-07-29 20:14:57 -0600

    Update styling to better vertically center the plus sign

--- src/components/AddTaskButton.js
+++ src/components/AddTaskButton.js
@@ -10,6 +10,7 @@ export default function FloatingActionButtons() {
         display: 'flex',
         justifyContent: 'center',
         // alignItems: 'center', // for some reason having this makes the text move lower and not look vertically centered
+        fontFamily: 'initial', // our font family also seems to lower the text, but if we override it back to the initial font family, it seems to vertically center better
         width: '50px',
         borderRadius: '100%',
         height: '50px',
