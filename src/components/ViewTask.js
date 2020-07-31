fb4e85c319978a7bc20d4bf6512ca2f3dc182ca3
Wade-Coplen <wadecoplen@gmail.com>
2020-07-31 06:00:59 -0700

    added axiosWithAuth

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -1,7 +1,15 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const ViewTask = () => {
+  axiosWithAuth()
+    .get(
+      'https://lyfe-logger-be.herokuapp.com/api/tasks/findById/user=:user_id/:task_id'
+    )
+    .then((res) => console.log('Hello from the console', res))
+    .catch((err) => console.log('Error', err));
+
   return (
     <div>
       <h2>Due Date</h2>
