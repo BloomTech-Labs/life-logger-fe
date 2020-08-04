2995140bdcda693f1d1866bf31fade4a6e5c9195
Wade-Coplen <wadecoplen@gmail.com>
2020-08-03 20:03:51 -0700

    reverted to formik form code

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -1,20 +1,96 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
+const initialValues = {
+  dueDate: new Date(),
+  description: '',
+};
+
+const onSubmit = (values) => {
+  console.log('form data', values);
+};
+const validationSchema = Yup.object().shape({
+  dueDate: Yup.date().required('Required').nullable(),
+  description: Yup.string().required('Required!'),
+});
 const ViewTask = () => {
   axiosWithAuth()
-    .get(
-      'https://lyfe-logger-be.herokuapp.com/api/tasks/findById/user=:user_id/:task_id'
-    )
-    .then((res) => console.log('Hello from the console', res))
-    .catch((err) => console.log('Error', err));
+    .get('https://lyfe-logger-be.herokuapp.com/api/tasks/findByUserId/:user_id')
+    .then((res) => {
+      console.log('Success', res);
+    })
+    .catch((err) => {
+      console.log('Not good', err);
+    });
+
+  const viewTaskFormStyle = {
+    display: 'block',
+    justifyContent: 'center',
+    marginTop: '20%',
+  };
+  const labelStyle = {
+    fontWeight: 'bold',
+    display: 'flex',
+    marginBottom: '5px',
+    marginLeft: '20%',
+  };
+  const inputStyle = {
+    width: '265px',
+    padding: '6 px 12px',
+    fontSize: '14px',
+    lineHeight: '1.5',
+    marginLeft: '20%',
+  };
+  const formControl = {
+    marginBottom: '20px',
+  };
+  const error = {
+    color: 'red',
+    marginLeft: '20%',
+  };
+  const completeTaskButton = {
+    marginLeft: '20%',
+  };
 
   return (
-    <div>
-      <h2>Due Date</h2>
-      <h2>Description</h2>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form sx={viewTaskFormStyle}>
+        <div style={formControl}>
+          <label style={labelStyle} htmlFor="Due Date">
+            Due Date
+          </label>
+          <Field style={inputStyle} type="date" id="due date" name="dueDate" />
+          <ErrorMessage style={error} name="dueDate" />
+        </div>
+        <br />
+        <div style={formControl}>
+          <label style={labelStyle} htmlFor="Description">
+            Description
+          </label>
+          <Field
+            style={inputStyle}
+            type="text"
+            id="description"
+            name="description"
+          />
+          <div style={error}>
+            <ErrorMessage name="description" />
+          </div>
+        </div>
+        <br />
+        <button style={completeTaskButton} type="completed">
+          Complete Task
+        </button>
+      </Form>
+    </Formik>
   );
 };
+
 export default ViewTask;
