17b50e4acedb0bea9ce7ce175b47a43889312b9f
Janessa Garrow <janessa.garrow@gmail.com>
2020-08-04 20:10:01 -0600

    Fix typo

--- src/utils/formatDate.test.js
+++ src/utils/formatDate.test.js
@@ -3,7 +3,7 @@ import { formatDate } from './formatDate';
 describe('formatDate tests', () => {
   it('returns a string with the date in YYYY-MM-DD format when given a Date object', () => {
     const expected = '2020-08-04';
-    const date = new Date('2020-08-05T02:03:43.970Z');
+    const date = new Date('2020-08-04T12:03:43.970Z');
     const actual = formatDate(date);
 
     expect(actual).toBe(expected);
