6b3ac6882e0ecc73271dce8327ad363e2b37f7d4
davidmoates <david.m.moates@gmail.com>
2020-08-10 20:18:44 -0500

    Broke Test

--- src/components/__tests__/LogoutButton.test.js
+++ src/components/__tests__/LogoutButton.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '../../../tests/contextProviderTestUtil';
+import LogoutButton from '../LogoutButton';
 import { Button } from '@theme-ui/components';
 
 describe('LogoutButton Task component tests', () => {
