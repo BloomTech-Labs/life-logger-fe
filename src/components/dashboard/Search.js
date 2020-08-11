18eeed18fa1aabf344634c8adf5409bb9d18f3a8
Jackie Atwood <takalaj@verizon.net>
2020-08-10 20:18:38 -0500

    Fixed broken test

--- src/components/dashboard/Search.js
+++ src/components/dashboard/Search.js
@@ -10,37 +10,34 @@ import Popper from '@material-ui/core/Popper';
 import TaskContext from '../../context/TaskContext';
 import MenuList from '@material-ui/core/MenuList';
 
-const FilterImg = styled.img `
-    height: 30px;
-    width: 30px;
-`
+const FilterImg = styled.img`
+  height: 30px;
+  width: 30px;
+`;
 
-const Container = styled.div `
-    display: flex;
-    justify-content: flex-start;
-    margin-top: -80px;
-    margin-right: 10px;
-`
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: -80px;
+  margin-right: 10px;
+`;
 
 export default function Search() {
-
-  const { tasks, setTasks } = React.useContext(TaskContext);
-  const [searchTerm, setSearchTerm] = React.useState("");
+  const { tasks } = React.useContext(TaskContext);
+  const [searchTerm, setSearchTerm] = React.useState('');
   const [open, setOpen] = React.useState(false);
   const [searchResults, setSearchResults] = React.useState([]);
   const anchorRef = React.useRef(null);
 
-  console.log("Task", tasks);
+  console.log('Task', tasks);
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
   React.useEffect(() => {
-    const results = tasks.filter(task => 
-      task.includes(searchTerm)
-    );
-      setSearchResults(results);
+    const results = tasks.filter((task) => task.includes(searchTerm));
+    setSearchResults(results);
   }, [searchTerm]);
 
   const handleToggle = () => {
@@ -73,42 +70,58 @@ export default function Search() {
     <div>
       <div>
         <Container>
-            <FilterImg
-                ref={anchorRef}
-                aria-controls={open ? 'menu-list-grow' : undefined}
-                aria-haspopup="true"
-                onClick={handleToggle}
-                src="./search.png"
-               >
-            </FilterImg>
-            <Popper open={open} role={undefined} transition disablePortal style={{ marginTop: '85px', marginLeft: '60px', height: '30px'}}>
+          <FilterImg
+            ref={anchorRef}
+            aria-controls={open ? 'menu-list-grow' : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+            src="./search.png"
+          ></FilterImg>
+          <Popper
+            open={open}
+            role={undefined}
+            transition
+            disablePortal
+            style={{ marginTop: '85px', marginLeft: '60px', height: '30px' }}
+          >
             {({ TransitionProps, placement }) => (
-                <Grow 
+              <Grow
                 {...TransitionProps}
-                style={{ transformOrigin: placement === 'bottom' ? 'right top' : 'right bottom' }}
-                >
+                style={{
+                  transformOrigin:
+                    placement === 'bottom' ? 'right top' : 'right bottom',
+                }}
+              >
                 <Paper>
-                    <ClickAwayListener onClickAway={handleClose}>
-                    <MenuList style={{ backgroundColor: "white", display: 'flex', justifyContent: 'center', flexDirection: 'column'}} autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                        <form>
-                          <input 
-                            type="text"
-                            placeholder="Search..."
-                            value={searchTerm}
-                            onChange={handleChange}
-                          />
-                        </form>
-                        {searchResults.map(item => (
-                          item.task
-                        ))}
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      style={{
+                        backgroundColor: 'white',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        flexDirection: 'column',
+                      }}
+                      autoFocusItem={open}
+                      id="menu-list-grow"
+                      onKeyDown={handleListKeyDown}
+                    >
+                      <form>
+                        <input
+                          type="search"
+                          placeholder="Search..."
+                          value={searchTerm}
+                          onChange={handleChange}
+                        />
+                      </form>
+                      {searchResults.map((item) => item.task)}
                     </MenuList>
-                    </ClickAwayListener>
+                  </ClickAwayListener>
                 </Paper>
-                </Grow>
+              </Grow>
             )}
-            </Popper>
-            </Container>
+          </Popper>
+        </Container>
       </div>
-      </div>
+    </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,36 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import styled from '@emotion/styled';
-import BurgerMenu from "../BurgerMenu";
+import BurgerMenu from '../BurgerMenu';
 
-const HeaderDiv = styled.div `
+const HeaderDiv = styled.div`
   background: #002550;
   height: 10vh;
   display: flex;
   justify-content: flex-start;
-`
+`;
 
-const Logo = styled.img `
+const Logo = styled.img`
   width: 40px;
   height: 50px;
   margin-left: 15px;
   margin-top: 15px;
-`
+`;
 
-const Title = styled.h2 `
+const Title = styled.h2`
   color: white;
   margin: 0 auto;
   display: flex;
   align-items: center;
   margin-left: 130px;
-`
+`;
 
 const Header = () => {
   return (
     <HeaderDiv>
       <Logo src="/OrangeIcon.png" />
-      <BurgerMenu/>
-      <Title>Life Logger</Title>
+      <BurgerMenu />
+      <Title data-testid="testtag">Life Logger</Title>
     </HeaderDiv>
   );
 };
