889dc1c43c222682a6babea98227133e66827f80
Jackie Atwood <takalaj@verizon.net>
2020-08-10 21:46:11 -0500

    Filter and Search Functions

--- src/components/dashboard/TaskList.js
+++ src/components/dashboard/TaskList.js
@@ -1,22 +1,28 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { useContext, useEffect } from 'react';
-import Search from "./Search";
+import Search from './Search';
 
 import TaskContext from '../../context/TaskContext';
 
 import Task from './Task';
 
 const TaskList = () => {
-  const { tasks, filter, getTasks } = useContext(TaskContext);
+  const { tasks, getTasks, searchTerm } = useContext(TaskContext);
 
-  const filterTasks = (task) => {
-    if (filter === null) {
+  // const filterTasks = (task) => {
+  //   if (filter === null) {
+  //     return task;
+  //   } else if (filter === 'incomplete') {
+  //     return task.is_complete === false;
+  //   } else if (filter === 'complete') {
+  //     return task.is_complete === true;
+  //   }
+  // };
+
+  const searchingFor = (task) => {
+    if (task.task_name.includes(searchTerm)) {
       return task;
-    } else if (filter === 'incomplete') {
-      return task.is_complete === false;
-    } else if (filter === 'complete') {
-      return task.is_complete === true;
     }
   };
 
@@ -28,18 +34,19 @@ const TaskList = () => {
 
   return (
     <div>
-    <Search />
-    <div
-      sx={{
-        display: `grid`,
-        gridTemplateColumns: [`1fr`, `minmax(auto, 400px)`],
-        gridGap: `15px`,
-      }}
-    >
-      {tasks.filter(filterTasks).map((task) => (
-        <Task key={task.id} task={task} />
-      ))}
-    </div>
+      <Search />
+      <div
+        sx={{
+          display: `grid`,
+          gridTemplateColumns: [`1fr`, `minmax(auto, 400px)`],
+          gridGap: `15px`,
+        }}
+      >
+        {/* {tasks.filter(filterTasks).map((task) => ( */}
+        {tasks.filter(searchingFor).map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </div>
     </div>
   );
 };
