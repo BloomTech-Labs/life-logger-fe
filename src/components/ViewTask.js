40eb8c98a320d5aead4ae58cb427195d8323d44c
Janessa Garrow <janessa.garrow@gmail.com>
2020-08-13 21:48:59 -0600

    Undo changes when pulled from another branch

--- src/components/ViewTask.js
+++ src/components/ViewTask.js
@@ -1,122 +1,51 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import PropTypes from 'prop-types';
-import { GrCalendar } from 'react-icons/gr';
-import { GrNotes } from 'react-icons/gr';
-import { GrTask } from 'react-icons/gr';
+import { AiTwotoneCalendar } from 'react-icons/ai';
 
 function ViewTask({ task }) {
+  const h3Div = {
+    display: 'flex',
+  };
+  const AiTwoTone = {
+    paddingTop: '25px',
+  };
+  const dueDate = {
+    paddingLeft: '20px',
+    color: '#b6b6b6',
+  };
+  const dueDateActual = {
+    paddingLeft: '40px',
+    marginTop: '5px',
+  };
+  const categoryName = {
+    paddingLeft: '35px',
+  };
+
   return (
     <div>
-      <h1
-        sx={{
-          //just messing around. Remove this is you do not like it.
-          borderBottomWidth: '1px',
-          borderBottomColor: 'black',
-          borderBottomStyle: 'solid',
-        }}
-      >
-        {task.task_name}
-      </h1>
-
-      <div
-        sx={{
-          display: 'flex',
-        }}
-      >
-        <div
-          sx={{
-            paddingTop: '1.5rem',
-          }}
-        >
-          <GrCalendar />
+      <h1>{task.task_name}</h1>
+      <div sx={h3Div}>
+        <div sx={AiTwoTone}>
+          <AiTwotoneCalendar />
         </div>
 
         <div>
-          <h3
-            sx={{
-              paddingLeft: '1rem',
-              color: 'black',
-            }}
-          >
-            Due Date
-          </h3>
-          <p
-            sx={{
-              paddingLeft: '1rem',
-              marginTop: '5px',
-            }}
-          >
-            {task.due_date}
-          </p>
+          <h3 sx={dueDate}>Due Date</h3>
         </div>
       </div>
 
-      <div
-        sx={{
-          display: 'flex',
-        }}
-      >
-        <div
-          sx={{
-            paddingTop: '1.5rem',
-          }}
-        >
-          <GrTask />
-        </div>
+      <p sx={dueDateActual}>{task.due_date}</p>
 
-        <div>
-          <h3
-            sx={{
-              paddingLeft: '1rem',
-              color: 'black',
-            }}
-          >
-            Category
-          </h3>
-          <p
-            sx={{
-              paddingLeft: '1rem',
-              marginTop: '5px',
-            }}
-          >
-            {task.category_name}
-          </p>
-        </div>
+      <p>{task.category_name}</p>
+
+      <div>
+        <h3 sx={categoryName}>Category Name: </h3>
       </div>
 
-      <div
-        sx={{
-          display: 'flex',
-        }}
-      >
-        <div
-          sx={{
-            paddingTop: '1.5rem',
-          }}
-        >
-          <GrNotes />
-        </div>
+      <h3>Task Notes:</h3>
 
-        <div>
-          <h3
-            sx={{
-              paddingLeft: '1rem',
-              color: 'black',
-            }}
-          >
-            Task Notes
-          </h3>
-          <p
-            sx={{
-              paddingLeft: '1rem',
-              marginTop: '5px',
-            }}
-          >
-            {task.task_notes}
-          </p>
-        </div>
-      </div>
+      <p>{task.task_notes}</p>
     </div>
   );
 }
